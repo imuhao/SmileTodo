02ddfdb5103b56282b33624a37b8ee53f19c7332
Smile <caimuhao@gmail.com>
2016-11-05 10:50:37 +0800

    添加Splash页 删除无用资源

---
+++ app/src/main/java/com/imuhao/smiletodo/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.imuhao.smiletodo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by smile on 16-11-5.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/imuhao/smiletodo/utils/UiHelper.java
+++ app/src/main/java/com/imuhao/smiletodo/utils/UiHelper.java
@@ -1,14 +1,11 @@
 package com.imuhao.smiletodo.utils;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 import com.imuhao.smiletodo.R;
